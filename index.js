fe8d8a3fa87fce3f724b0ddbfdb5bc67d238dd41
Codam <damisco005@gmail.com>
2023-11-07 19:14:12 +0100

    login page

--- index.js
+++ index.js
@@ -8,15 +8,22 @@ app.get('/', (req, res)=>{
     res.render('index')
 })
 
-app.get('/login', (req, res)=>{
+app.get('/student/login', (req, res)=>{
     res.render('login')
 })
-
 app.get('/student/dashboard', (req, res)=>{
     res.render('studentDashboard')
 })
-app.get('/logout', (req, res)=>{
-    res.redirect('/login')
+
+app.get('/admin/login', (req, res)=>{
+    res.render('adminDashboard')
+})
+app.get('/admin/dashboard', (req, res)=>{
+    res.render('studentDashboard')
+})
+
+app.get('/student/logout', (req, res)=>{
+    res.redirect('/student/login')
 })
 app.use((req, res)=>{res.render('404', {req})})
 app.listen(8000, ()=>console.log("server started at port 8000"))
--- views/public/javascript/sDashboard.js
+++ views/public/javascript/sDashboard.js
@@ -9,32 +9,74 @@ const showForm = document.querySelector('.contain-form')
 
 const form = document.getElementById('complain')
 
+
+const images = document.querySelector('.image')
+const documents = document.querySelector('.documents')
+const texts = document.querySelector('.text')
+const course = document.querySelector('.mother-course')
+const main = document.querySelector('.main .content')
+const mother_message = document.querySelector('.mother-messages')
+const initial_message = document.querySelector('.initial-message')
+console.log(documents);
+
+function toggleNone(params) {
+    for (let i = 0; i < main.children.length; i++) {
+        const element = main.children[i];
+        element.style.display = 'none'
+    }
+    console.log(params);
+    if (params)
+    {
+        mother_message.style.display = 'block'
+        for (let i = 0; i < mother_message.children.length; i++) {
+            const element = mother_message.children[i];
+            element.style.display = 'none'
+        }
+    }
+}
+
 form.addEventListener('click', (e)=>{
     e.stopPropagation()
 })
 appear[0].addEventListener('click', ()=>{
     messageStatus[0].classList.toggle('contain-msg')
+    toggleNone()
+    initial_message.style.display = 'grid'
 })
 appear[1].addEventListener('click', ()=>{
     messageStatus[1].classList.toggle('contain-courses')
+    toggleNone()
+    course.style.display = 'grid'
 })
 settings.addEventListener('click', ()=>{
     showSettings.classList.toggle('setting-preview-hide')
 })
 sendMessage.addEventListener('click', ()=>{
     showForm.classList.toggle('hide')
+    toggleNone()
 })
 showForm.addEventListener('click', (e)=>{
     showForm.classList.toggle('hide')
-    e.stopPropagation()
+    toggleNone()
+})
+const element = messageStatus[0].children
+console.log(element);
+
+element[0].addEventListener('click', (e)=>{
+    toggleNone('messages')
+    images.style.display = 'block'
+})
+element[1].addEventListener('click', (e)=>{
+    toggleNone('messages')
+    documents.style.display = 'grid'
+})
+element[2].addEventListener('click', (e)=>{
+    toggleNone('messages')
+    texts.style.display = 'block'
 })
 
-const course = document.querySelector('.mother-course')
-const images = document.querySelector('.image')
-const documents = document.querySelector('.documents')
-const texts = document.querySelector('.text')
 
-const images_status = getComputedStyle(images)
-const documents_status = getComputedStyle(documents)
-const texts_status = getComputedStyle(texts)
+
+
+
 
